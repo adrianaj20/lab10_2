b79e9dc93cf29f637e302539bf2d0058d8bb73f8
Alex <alex@example.com>
2022-11-08 15:06:12 -0500

    commit najarro semi

---
+++ src/main/java/com/tecsup/petclinic/services/OwnerService.java
@@ -0,0 +1,43 @@
+package com.tecsup.petclinic.services;
+
+import java.util.List;
+import com.tecsup.petclinic.entities.Owner;
+import com.tecsup.petclinic.exception.OwnerNotFoundException;
+
+public interface OwnerService {
+
+    /**
+     * 
+     * @param owner
+     * @return
+     */
+    Owner create(Owner owner);
+
+    /**
+     * 
+     * @param pet
+     * @return
+     */
+    Owner update(Owner owner);
+
+    /**
+     * 
+     * @param id
+     */
+    void delete(Long id);
+
+    /**
+     * 
+     * @param id
+     * @return
+     */
+    Owner findById(long id) throws OwnerNotFoundException;
+
+    /**
+     * 
+     * @param name
+     * @return
+     */
+    List<Owner> findByFirstname(String firstname);
+
+}
---
+++ src/main/java/com/tecsup/petclinic/services/OwnerServiceImpl.java
@@ -0,0 +1,48 @@
+package com.tecsup.petclinic.services;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tecsup.petclinic.entities.Owner;
+import com.tecsup.petclinic.exception.OwnerNotFoundException;
+import com.tecsup.petclinic.repositories.OwnerRepository;
+
+@Service
+public class OwnerServiceImpl implements OwnerService {
+
+    @Autowired
+    OwnerRepository ownerRepository;
+
+    @Override
+    public Owner create(Owner owner) {
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public Owner update(Owner owner) {
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public void delete(Long id) {
+        ownerRepository.deleteById(id);
+    }
+
+    @Override
+    public Owner findById(long id) throws OwnerNotFoundException {
+        Optional<Owner> owner = ownerRepository.findById(id);
+        if (!owner.isPresent())
+            throw new OwnerNotFoundException("Record not found...!");
+        return owner.get();
+    }
+
+    @Override
+    public List<Owner> findByFirstname(String firstname) {
+        return ownerRepository.findByFirstname(firstname);
+    }
+
+}
