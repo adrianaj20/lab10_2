625c05a480dd1a03009b62fa270db4b1e8079c53
Lesandrod <lentrix12345@gmail.com>
2022-11-08 15:32:55 -0500

    Owner servicetest

--- src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
@@ -1,39 +1,42 @@
 package com.tecsup.petclinic.services;
 
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.tecsup.petclinic.entities.Owner;
+import com.tecsup.petclinic.exception.OwnerNotFoundException;
 
 @SpringBootTest
 public class OwnerServiceTest {
 
 	private static final Logger logger = LoggerFactory.getLogger(OwnerServiceTest.class);
 
-	//@Autowired
-   //	private OwnerService ownerService;
+	@Autowired
+	private OwnerService ownerService;
 
-	/**
-	 * 
-	 */
-	/*
 	@Test
 	public void testFindOwnerById() {
 
 		long ID = 1;
-		String NAME = "Jaime";
+		String firstname = "George";
+
 		Owner owner = null;
-		
+
 		try {
-			
 			owner = ownerService.findById(ID);
-			
-		} catch (OwnertNotFoundException e) {
+		} catch (OwnerNotFoundException e) {
 			fail(e.getMessage());
 		}
-		logger.info("" + owner);
 
-		assertEquals(NAME, owner.getName());
+		logger.info("" + owner);
+		assertThat(owner.getFirstname(), is(firstname));
 
 	}
-	*/
 }
