4c778b333ac4c309d2e8fc9be31738e0e93d1859
Lesandrod <lentrix12345@gmail.com>
2022-11-08 15:15:23 -0500

    repository - Idone

--- src/main/java/com/tecsup/petclinic/repositories/OwnerRepository.java
+++ src/main/java/com/tecsup/petclinic/repositories/OwnerRepository.java
@@ -1,11 +1,11 @@
 package com.tecsup.petclinic.repositories;
 
-import java.util.List;
+import java.util.*;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.tecsup.petclinic.entities.Owner;
+import com.tecsup.petclinic.entities.*;
 
 @Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
