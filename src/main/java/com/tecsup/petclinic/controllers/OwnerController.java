c2b363a984d7f9a1cf55b0054b69893c408a7952
Lesandrod <lentrix12345@gmail.com>
2022-11-15 15:30:54 -0500

    comii

--- src/main/java/com/tecsup/petclinic/controllers/OwnerController.java
+++ src/main/java/com/tecsup/petclinic/controllers/OwnerController.java
@@ -47,7 +47,7 @@ public class OwnerController {
 	 * @return
 	 * @throws OwnerNotFoundException
 	 */
-    @GetMapping("/pets/{id}")
+    @GetMapping("/owners/{id}")
     ResponseEntity<Owner> findOne(@PathVariable Long id) {
         try {
             return new ResponseEntity<>(ownerService.findById(id), HttpStatus.OK);
